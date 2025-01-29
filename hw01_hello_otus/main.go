2318dbd15ffc0302cb95e187ecbb5c83e9883b83
Kirill Novozhilov <knovozhilov@optimalgroup.ru>
2025-01-29 23:53:43 +0400

    feat: hw1

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	phrase := "Hello, OTUS!"
+
+	fmt.Println(reverse.String(phrase))
 }
