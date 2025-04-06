55b72fd203a25dc7fd3e0f54d78e0acc813c555c
Kirill Novozhilov <knovozhilov@optimalgroup.ru>
2025-04-06 21:08:50 +0400

    feat: hw4

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,96 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	length int
+	front  *ListItem
+	back   *ListItem
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.length
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newItem := &ListItem{Value: v}
+
+	if l.length == 0 {
+		l.front = newItem
+		l.back = newItem
+	} else {
+		newItem.Next = l.front
+		l.front.Prev = newItem
+		l.front = newItem
+	}
+
+	l.length++
+	return newItem
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newItem := &ListItem{Value: v}
+
+	if l.length == 0 {
+		l.front = newItem
+		l.back = newItem
+	} else {
+		newItem.Prev = l.back
+		l.back.Next = newItem
+		l.back = newItem
+	}
+
+	l.length++
+	return newItem
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i == nil {
+		return
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.front = i.Next
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.back = i.Prev
+	}
+
+	l.length--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == nil || i == l.front {
+		return
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.back = i.Prev
+	}
+
+	i.Prev = nil
+	i.Next = l.front
+	l.front.Prev = i
+	l.front = i
 }
